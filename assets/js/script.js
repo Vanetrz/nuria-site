5ce26c9d74e92922a60576fa8a4a0443d0ff5623
vanessa <vanessa.trz.amorim@gmail.com>
2024-11-25 23:31:18 -0300

    func-caderno

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // seta para baixo
 const seta = document.querySelector('#seta')
+const carregaimg = ["assets/img/anotacao_branco.png", "assets/img/caderno_roxo.png", "assets/img/anotacao_roxo.png", "assets/img/caderno_branco.png"];
+
 
 seta.addEventListener('click', () => {
     window.scroll({top: window.innerHeight})
@@ -22,58 +24,63 @@ setaCima.addEventListener ('click', () => {
     })
 })
 
-// function checkHeight() {
-//     if(Window.scrollY > 200) {
-//         setaCima.style.display = "flex"
-//     } else {
-//         setaCima.style.display = "none"
-//     }
-// }
+//AREA DAS ANOTAÇÕES E CADERNO
+const caderno = "caderno";
+const anotacoes = "anotacoes";
+var modo_caderno_anotacao = caderno;
 
-// carrossel
-// const imagens = document.querySelectorAll('.imagens');
-// const text = document.querySelectorAll('.text');
-// const btnBack = document.getElementById('back-button');
-// const btnNext = document.getElementById('next-button');
+function caderno_select()
+{
+    //Caderno selecionado
+    let anotacoes_select = document.getElementById("anotacoes-select");
+    let caderno_select = document.getElementById("caderno-select");
+    let imagem_grande = document.getElementById("imagem-grande");
 
-// // passador do slide (soma=passa, sub=volta)
-// let currentSlide = 0;
+    anotacoes_select.src = "assets/img/anotacoes_branco.png";
+    caderno_select.src = "assets/img/caderno_roxo.png";
 
-// // função de esconder o carrossel
-// function hideImagens () {
-//     imagens.forEach(item => item.classList.remove('on'))
-//     text.forEach(item => item.classList.remove('on'))
-// }
+    imagem_grande.src = "assets/img/caderno-grande.png";
 
-// // função de mostrar o carrossel
-// function showImagens() {
-//     imagens[currentSlide].classList.add('on')
-//     text[currentSlide].classList.add('on')
-// }
+    modo_caderno_anotacao = caderno; 
+}
 
-// // função de avançar
-// function nextImagem() {
-//     hideImagens()
-//     if(currentSlide === imagens.length -1) {
-//         currentSlide = 0
-//     } else {
-//         currentSlide++
-//     }
-//     showImagens()
-// }
+function anotacoes_select()
+{
+    //Anotações selecionado
+    let anotacoes_select = document.getElementById("anotacoes-select");
+    let caderno_select = document.getElementById("caderno-select");
+    let imagem_grande = document.getElementById("imagem-grande");
+    
+    anotacoes_select.src = "assets/img/anotacoes_roxo.png";
+    caderno_select.src = "assets/img/caderno_branco.png";
 
-// // função de voltar
-// function backImagem() {
-//     hideImagens()
-//     if(currentSlide === 0) {
-//         currentSlide = imagens.length -1
-//     } else {
-//         currentSlide--
-//     }
-//     showImagens()
-// }
+    imagem_grande.src = "assets/img/anotacao-grande.png";
 
-// btnBack.addEventListener('click', nextImagem)
-// btnNext.addEventListener('click', backImagem)
+    modo_caderno_anotacao = anotacoes;
+}
 
-// console.log(imagens)
+//Planner mudando de imagem
+const desempenho = "desempenho";
+const inteligente = "inteligente";
+const integracao = "integracao";
+var imagem_grande = document.getElementById("image-planner");
+
+var modo_planner = desempenho;
+
+function planner_desempenho()
+{
+    imagem_grande.src = "assets/img/planner.gif";
+    imagem_grande.style.height = "754px";
+}
+
+function planner_inteligente()
+{
+    imagem_grande.src = "assets/img/filtrar.gif";
+    imagem_grande.style.height = "754px";
+}
+
+function planner_integracao()
+{
+    imagem_grande.src = "assets/img/calendario.gif";
+    imagem_grande.style.height = "754px";
+}
